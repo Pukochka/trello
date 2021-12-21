77363c58a29e2304e0d64f099a50af1e54720980
pukochka <tema.puko@gmail.com>
2021-12-21 19:09:48 +0300

    fix slider

--- script.js
+++ script.js
@@ -403,69 +403,93 @@
         });
 
         function mobileSlider(){
-            const slides = Array.from(document.querySelectorAll('.trello-item'));
 
-            let isDragging = false,
+                // set up our state
+                let isDragging = false,
                 startPos = 0,
                 currentTranslate = 0,
                 prevTranslate = 0,
-                currentSlide = 0;
+                animationID,
+                currentSlide = 0
 
-            slides.forEach((slide, index) => {
-
-                slide.addEventListener('touchstart', touchStart(index));
-                slide.addEventListener('touchend', touchEnd);
-                slide.addEventListener('touchmove', touchMove);
-
-                slide.addEventListener('mousedown', touchStart(index));
-                slide.addEventListener('mouseup', touchEnd);
-                slide.addEventListener('mousemove', touchMove);
-                slide.addEventListener('mouseleave', touchEnd);
+                // add our event listeners
+                caruseilSlides.forEach((slide, index) => {
                 
-            });
+                // disable default image drag
+                
+                // touch events
+                slide.addEventListener('touchstart', touchStart(index))
+                slide.addEventListener('touchend', touchEnd)
+                slide.addEventListener('touchmove', touchMove)
+                // mouse events
+                slide.addEventListener('mousedown', touchStart(index))
+                slide.addEventListener('mouseup', touchEnd)
+                slide.addEventListener('mousemove', touchMove)
+                slide.addEventListener('mouseleave', touchEnd)
+                })
 
-            function getPositionX(event) {
-                return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
-            }
+                // make responsive to viewport changes
+                window.addEventListener('resize', setPositionByIndex)
 
-            
-            function touchStart(index) {
-                return function (event) {
-                    currentSlide = index;
-                    startPos = getPositionX(event);
-                    isDragging = true;
+                // prevent menu popup on long press
+                window.oncontextmenu = function (event) {
+                event.preventDefault()
+                event.stopPropagation()
+                return false
                 }
-            }
 
-            function touchMove(event) {
-                if (isDragging) {
+                function getPositionX(event) {
+                return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+                }
 
-                    const currentPosition = getPositionX(event);
-                    currentTranslate = prevTranslate + currentPosition - startPos;
-
-                    changeSlide(currentSlide);
-
+                // use a HOF so we have index in a closure
+                function touchStart(index) {
+                return function (event) {
+                    currentSlide = index
+                    startPos = getPositionX(event)
+                    isDragging = true
+                    animationID = requestAnimationFrame(animation)
                     activeDot(currentSlide);
                 }
-            }
+                }
 
-            function touchEnd() {
-                
-                isDragging = false;
-                const movedBy = currentTranslate - prevTranslate;
+                function touchMove(event) {
+                if (isDragging) {
+                    const currentPosition = getPositionX(event)
+                    currentTranslate = prevTranslate + currentPosition - startPos
+                }
+                }
 
-                if (movedBy < -100 && currentSlide < slides.length - 1) {
-                    currentSlide += 1
-                };
+                function touchEnd() {
+                cancelAnimationFrame(animationID)
+                isDragging = false
+                const movedBy = currentTranslate - prevTranslate
 
-                if (movedBy > 100 && currentSlide > 0) {
-                    currentSlide -= 1
-                };
-                
-                changeSlide(currentSlide);
+                // if moved enough negative then snap to next slide if there is one
+                if (movedBy < -100 && currentSlide < caruseilSlides.length - 1) currentSlide += 1
+
+                // if moved enough positive then snap to previous slide if there is one
+                if (movedBy > 100 && currentSlide > 0) currentSlide -= 1
+
+                setPositionByIndex()
 
                 activeDot(currentSlide);
-            }   
+                }
+
+                function animation() {
+                setSliderPosition()
+                if (isDragging) requestAnimationFrame(animation)
+                }
+
+                function setPositionByIndex() {
+                currentTranslate = currentSlide * -window.innerWidth
+                prevTranslate = currentTranslate
+                setSliderPosition()
+                }
+
+                function setSliderPosition() {
+                    sliderTrack.style.transform = `translateX(${currentTranslate}px)`
+                }  
         }
     }
     
